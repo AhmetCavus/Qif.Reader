0ca5bf11da9e45d9fc3e1fda47142a119e18e675
Ahmet Cavus <cavus.ahmet@outlook.com>
2020-12-29 20:51:07 +0100

    Adding a necessary integration test

---
+++ Cinary.Finance.Qif.IntegrationTest/Service/QifServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using Cinary.Finance.Qif.Repository;
+using Cinary.Finance.Qif.Service.Interface;
+using Cinary.Finance.Qif.TransactionTypes;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Cinary.Finance.Qif.IntegrationTest.Service
+{
+    public class QifServiceTests
+    {
+        string _qifFilePath = "/export.qif";
+        IQifService _qifService;
+
+        [SetUp]
+        public void Init()
+        {
+            // Arrange
+            _qifFilePath = $"{Environment.CurrentDirectory}\\export.qif";
+            _qifService = new QifService(new QifReader(), new QifRepositoryContainer());
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+            _qifService.Dispose();
+        }
+
+
+        [Test]
+        [Explicit("Depends on a qif file. This file had to be provided before executing this test")]
+        public async Task QifService_QueryFromFile_CheckIfResultIsNotNull()
+        {
+            // Arrange
+
+
+            // Act
+            var transactions = await _qifService.QueryFromFileAsync<NonInvestmentTransaction>(_qifFilePath);
+
+            // Assert
+            Assert.NotNull(transactions);
+        }
+
+
+    }
+}
