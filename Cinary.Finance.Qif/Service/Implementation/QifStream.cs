abfd780b160cb46efa93e5f2345dba69cfdb321e
Ahmet Cavus <cavus.ahmet@outlook.com>
2020-12-30 01:53:06 +0100

    Adding support for multiple Accounts

--- Cinary.Finance.Qif/Service/Implementation/QifStream.cs
+++ Cinary.Finance.Qif/Service/Implementation/QifStream.cs
@@ -24,7 +24,11 @@ namespace Cinary.Finance.Qif
             _in.BaseStream.Position = 0;
 
             // Read header line
-            var header = _in.ReadLine();
+            string header = string.Empty;
+            do
+            {
+                header = _in.ReadLine();
+            } while (!IsHeaderValid(header));
 
             // Get transaction type
             var transactionType = string.Empty;
@@ -76,15 +80,60 @@ namespace Cinary.Finance.Qif
 
         public async Task<IList<ITransactionEntry>> ReadTransactionsAsync<T>() where T : ITransactionEntry
         {
+            var mapper = ResolveMapper<T>();
+
             // Go to start
             _in.BaseStream.Position = 0;
 
+            var transactions = new List<ITransactionEntry>();
+            while (!_in.EndOfStream)
+            {
+                var transactionType = await ResolveTransactionType();
+
+                while (!_in.EndOfStream)
+                {
+                    var line = string.Empty;
+                    var transactionString = string.Empty;
+                    while(!IsNextTransaction(line) 
+                        && !IsNewTransactionType(line))
+                    {
+                        line = await _in.ReadLineAsync();
+                        transactionString += line + Environment.NewLine;
+                    }
+                    CreateAndAddTransaction(transactions, mapper, transactionType, transactionString);
+                    if (IsNewTransactionType(line))
+                    {
+                        _in.BaseStream.Position -= 1024;
+                        break;
+                    }
+                }
+            }
+            return transactions;
+        }
+
+        void CreateAndAddTransaction<TType>(IList<ITransactionEntry> transactions, TransactionMap<TType> mapper, string transactionType, string transactionString) where TType : ITransactionEntry
+        {
+            if (transactionString.StartsWith("!")) return;
+            var transactionToAdd = CreateTransaction(mapper, transactionType, transactionString);
+            transactions.Add((TType)Convert.ChangeType(transactionToAdd, typeof(TType)));
+        }
+
+        TType CreateTransaction<TType>(TransactionMap<TType> mapper, string transactionType, string transactionString) where TType : ITransactionEntry
+        {
+            // Parse, map and add to listi
+            var transaction = mapper.Parse<TType>(transactionString);
+            transaction.Type = transactionType;
+            return transaction;
+        }
+
+        async Task<string> ResolveTransactionType()
+        {
             // Read header line
             string header = string.Empty;
-            do
+            while (!IsHeaderValid(header) && !_in.EndOfStream)
             {
                 header = await _in.ReadLineAsync();
-            } while (!IsHeaderValid(header));
+            }
 
             // Get transaction type
             var transactionType = string.Empty;
@@ -96,45 +145,34 @@ namespace Cinary.Finance.Qif
             {
                 throw new FormatException("Invalid Header format;");
             }
+            return transactionType;
+        }
 
-            // Go to start
-            _in.BaseStream.Position = 1;
+        TransactionMap<TType> ResolveMapper<TType>() where TType : ITransactionEntry
+        {
+            var (mapperClass, mapperName) = ResolveMapperClass<TType>();
+            if (mapperClass == null)
+                throw new NotImplementedException("The class '" + mapperName + "' was not found.");
+            var mapper = (TransactionMap<TType>)Activator.CreateInstance(mapperClass.AsType());
+            return mapper;
+        }
 
-
-            // dummy proof
-            var transactions = new List<ITransactionEntry>();
-
+        (TypeInfo typeInfo, string mapperName) ResolveMapperClass<TType>()
+        {
             // Use convention to find the mapper
-            var mapperName = typeof(T).Name + "Map";
+            var mapperName = typeof(TType).Name + "Map";
             var assembly = GetType().GetTypeInfo().Assembly;
             var toFind = $"{GetType().Namespace}.Data.TransactionMapping.{mapperName}";
             var mapperClass = (from item in assembly.DefinedTypes
                                where item.FullName == toFind
                                select item).FirstOrDefault();
-
-            if (mapperClass == null)
-                throw new NotImplementedException("The class '" + mapperName + "' was not found.");
-
-            var mapper = (TransactionMap<T>)Activator.CreateInstance(mapperClass.AsType());
-
-            // Get the next transaction
-            string line = string.Empty;
-            while(_in.BaseStream.Position < _in.BaseStream.Length - 1)
-            {
-                var transaction = (T)Activator.CreateInstance(typeof(T));
-                string transactionstring = string.Empty;
-                while (!(line = await _in.ReadLineAsync()).StartsWith("^"))
-                {
-                    transactionstring += line + Environment.NewLine;
-                }
-                // Parse, map and add to list
-                transaction = mapper.Parse<T>(transactionstring);
-                transaction.Type = transactionType;
-                transactions.Add((T)Convert.ChangeType(transaction,typeof(T)));
-            }
-            return transactions;
+            return (mapperClass, mapperName);
         }
 
         bool IsHeaderValid(string header) => header != null && header.StartsWith("!Type:");
+
+        bool IsNextTransaction(string line) => line.StartsWith("^");
+
+        bool IsNewTransactionType(string line) => line.StartsWith("!Type:");
     }
 }
